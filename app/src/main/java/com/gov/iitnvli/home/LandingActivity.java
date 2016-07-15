34affd5ccb70aeb4f938173b63dd7afe3414f760
murtuza saifee <ermurtuzasaifee@gmail.com>
2016-07-15 16:58:00 +0400

    Logo added

--- app/src/main/java/com/gov/iitnvli/home/LandingActivity.java
+++ app/src/main/java/com/gov/iitnvli/home/LandingActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.github.florent37.materialviewpager.MaterialViewPager;
@@ -22,6 +23,8 @@ public class LandingActivity extends AppCompatActivity {
     private ActionBarDrawerToggle drawerToggle;
     private MaterialViewPager mViewPager;
     private Toolbar toolbar;
+    private ImageView headerLogo;
+    private int pageCount = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,8 @@ public class LandingActivity extends AppCompatActivity {
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerToggle = new ActionBarDrawerToggle(this, drawer, 0, 0);
         drawer.setDrawerListener(drawerToggle);
+
+        headerLogo = (ImageView) findViewById(R.id.headerLogo);
 
         final ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -55,7 +60,7 @@ public class LandingActivity extends AppCompatActivity {
 
             @Override
             public Fragment getItem(int position) {
-                switch (position % 4) {
+                switch (position % pageCount) {
                     //case 0:
                     //    return RecyclerViewFragment.newInstance();
                     //case 1:
@@ -69,20 +74,28 @@ public class LandingActivity extends AppCompatActivity {
 
             @Override
             public int getCount() {
-                return 4;
+                return pageCount;
             }
 
             @Override
             public CharSequence getPageTitle(int position) {
-                switch (position % 4) {
+                switch (position % pageCount) {
                     case 0:
-                        return "Selection";
+                        return getString(R.string.books);
                     case 1:
-                        return "Actualités";
+                        return getString(R.string.thesis);
                     case 2:
-                        return "Professionnel";
+                        return getString(R.string.museum);
                     case 3:
-                        return "Divertissement";
+                        return getString(R.string.archieves);
+                    case 4:
+                        return getString(R.string.audiovideo);
+                    case 5:
+                        return getString(R.string.manuscripts);
+                    case 6:
+                        return getString(R.string.newspaper);
+                    case 7:
+                        return getString(R.string.maps);
                 }
                 return "";
             }
@@ -93,24 +106,48 @@ public class LandingActivity extends AppCompatActivity {
             public HeaderDesign getHeaderDesign(int page) {
                 switch (page) {
                     case 0:
+                        headerLogo.setImageResource(R.drawable.book);
                         return HeaderDesign.fromColorResAndUrl(
-                                R.color.green,
+                                R.color.books,
                                 "https://fs01.androidpit.info/a/63/0e/android-l-wallpapers-630ea6-h900.jpg");
                     case 1:
+                        headerLogo.setImageResource(R.drawable.thesis);
                         return HeaderDesign.fromColorResAndUrl(
-                                R.color.blue,
+                                R.color.thesis,
                                 "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/06/wallpaper_51.jpg");
                     case 2:
+                        headerLogo.setImageResource(R.drawable.musem);
                         return HeaderDesign.fromColorResAndUrl(
-                                R.color.cyan,
+                                R.color.musem,
                                 "http://www.droid-life.com/wp-content/uploads/2014/10/lollipop-wallpapers10.jpg");
                     case 3:
+                        headerLogo.setImageResource(R.drawable.archieve);
                         return HeaderDesign.fromColorResAndUrl(
-                                R.color.red,
+                                R.color.archieve,
+                                "http://www.tothemobile.com/wp-content/uploads/2014/07/original.jpg");
+                    case 4:
+                        headerLogo.setImageResource(R.drawable.audiovideo);
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.audioVideo,
+                                "https://fs01.androidpit.info/a/63/0e/android-l-wallpapers-630ea6-h900.jpg");
+                    case 5:
+                        headerLogo.setImageResource(R.drawable.manuscript);
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.manuscript,
+                                "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/06/wallpaper_51.jpg");
+                    case 6:
+                        headerLogo.setImageResource(R.drawable.newspaper);
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.newspaper,
+                                "http://www.droid-life.com/wp-content/uploads/2014/10/lollipop-wallpapers10.jpg");
+                    case 7:
+                        headerLogo.setImageResource(R.drawable.map);
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.map,
                                 "http://www.tothemobile.com/wp-content/uploads/2014/07/original.jpg");
                 }
 
-                //execute others actions if needed (ex : modify your header logo)
+                //execute others actions if needed //
 
                 return null;
             }
