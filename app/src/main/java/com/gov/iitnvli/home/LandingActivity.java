e8d8778019ee893b514e7272844a72658ad433e4
murtuza saifee <ermurtuzasaifee@gmail.com>
2016-07-15 17:09:28 +0400

    Setup dynamic UI components

--- app/src/main/java/com/gov/iitnvli/home/LandingActivity.java
+++ app/src/main/java/com/gov/iitnvli/home/LandingActivity.java
@@ -1,6 +1,7 @@
 package com.gov.iitnvli.home;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -17,7 +18,7 @@ import com.github.florent37.materialviewpager.header.HeaderDesign;
 import com.gov.iitnvli.R;
 
 
-public class LandingActivity extends AppCompatActivity {
+public class LandingActivity extends AppCompatActivity implements MaterialViewPager.Listener {
 
     private DrawerLayout drawer;
     private ActionBarDrawerToggle drawerToggle;
@@ -31,13 +32,29 @@ public class LandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
 
-        setTitle("");
+        setupDrawer();
+        headerLogo = (ImageView) findViewById(R.id.headerLogo);
+        setupMaterialViewPager();
+    }
 
+    private void setupMaterialViewPager() {
+        mViewPager = (MaterialViewPager) findViewById(R.id.materialViewPager);
+        toolbar = mViewPager.getToolbar();
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        mViewPager.getViewPager().setAdapter(new CustomViewPagerAdapter(getSupportFragmentManager()));
+        mViewPager.setMaterialViewPagerListener(this);
+        mViewPager.getViewPager().setOffscreenPageLimit(mViewPager.getViewPager().getAdapter().getCount());
+        mViewPager.getPagerTitleStrip().setViewPager(mViewPager.getViewPager());
+    }
+
+    private void setupDrawer() {
+        setTitle("");
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerToggle = new ActionBarDrawerToggle(this, drawer, 0, 0);
         drawer.setDrawerListener(drawerToggle);
-
-        headerLogo = (ImageView) findViewById(R.id.headerLogo);
 
         final ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -47,121 +64,109 @@ public class LandingActivity extends AppCompatActivity {
             actionBar.setDisplayUseLogoEnabled(false);
             actionBar.setHomeButtonEnabled(true);
         }
-
-        mViewPager = (MaterialViewPager) findViewById(R.id.materialViewPager);
-
-        toolbar = mViewPager.getToolbar();
-
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
-        }
-
-        mViewPager.getViewPager().setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
-
-            @Override
-            public Fragment getItem(int position) {
-                switch (position % pageCount) {
-                    //case 0:
-                    //    return RecyclerViewFragment.newInstance();
-                    //case 1:
-                    //    return RecyclerViewFragment.newInstance();
-                    //case 2:
-                    //    return WebViewFragment.newInstance();
-                    default:
-                        return RecyclerViewFragment.newInstance();
-                }
-            }
-
-            @Override
-            public int getCount() {
-                return pageCount;
-            }
-
-            @Override
-            public CharSequence getPageTitle(int position) {
-                switch (position % pageCount) {
-                    case 0:
-                        return getString(R.string.books);
-                    case 1:
-                        return getString(R.string.thesis);
-                    case 2:
-                        return getString(R.string.museum);
-                    case 3:
-                        return getString(R.string.archieves);
-                    case 4:
-                        return getString(R.string.audiovideo);
-                    case 5:
-                        return getString(R.string.manuscripts);
-                    case 6:
-                        return getString(R.string.newspaper);
-                    case 7:
-                        return getString(R.string.maps);
-                }
-                return "";
-            }
-        });
-
-        mViewPager.setMaterialViewPagerListener(new MaterialViewPager.Listener() {
-            @Override
-            public HeaderDesign getHeaderDesign(int page) {
-                switch (page) {
-                    case 0:
-                        headerLogo.setImageResource(R.drawable.book);
-                        return HeaderDesign.fromColorResAndUrl(
-                                R.color.books,
-                                "https://fs01.androidpit.info/a/63/0e/android-l-wallpapers-630ea6-h900.jpg");
-                    case 1:
-                        headerLogo.setImageResource(R.drawable.thesis);
-                        return HeaderDesign.fromColorResAndUrl(
-                                R.color.thesis,
-                                "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/06/wallpaper_51.jpg");
-                    case 2:
-                        headerLogo.setImageResource(R.drawable.musem);
-                        return HeaderDesign.fromColorResAndUrl(
-                                R.color.musem,
-                                "http://www.droid-life.com/wp-content/uploads/2014/10/lollipop-wallpapers10.jpg");
-                    case 3:
-                        headerLogo.setImageResource(R.drawable.archieve);
-                        return HeaderDesign.fromColorResAndUrl(
-                                R.color.archieve,
-                                "http://www.tothemobile.com/wp-content/uploads/2014/07/original.jpg");
-                    case 4:
-                        headerLogo.setImageResource(R.drawable.audiovideo);
-                        return HeaderDesign.fromColorResAndUrl(
-                                R.color.audioVideo,
-                                "https://fs01.androidpit.info/a/63/0e/android-l-wallpapers-630ea6-h900.jpg");
-                    case 5:
-                        headerLogo.setImageResource(R.drawable.manuscript);
-                        return HeaderDesign.fromColorResAndUrl(
-                                R.color.manuscript,
-                                "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/06/wallpaper_51.jpg");
-                    case 6:
-                        headerLogo.setImageResource(R.drawable.newspaper);
-                        return HeaderDesign.fromColorResAndUrl(
-                                R.color.newspaper,
-                                "http://www.droid-life.com/wp-content/uploads/2014/10/lollipop-wallpapers10.jpg");
-                    case 7:
-                        headerLogo.setImageResource(R.drawable.map);
-                        return HeaderDesign.fromColorResAndUrl(
-                                R.color.map,
-                                "http://www.tothemobile.com/wp-content/uploads/2014/07/original.jpg");
-                }
-
-                //execute others actions if needed //
-
-                return null;
-            }
-        });
-
-        mViewPager.getViewPager().setOffscreenPageLimit(mViewPager.getViewPager().getAdapter().getCount());
-        mViewPager.getPagerTitleStrip().setViewPager(mViewPager.getViewPager());
-
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         drawerToggle.syncState();
+    }
+
+    @Override
+    public HeaderDesign getHeaderDesign(int page) {
+        switch (page) {
+            case 0:
+                headerLogo.setImageResource(R.drawable.book);
+                return HeaderDesign.fromColorResAndUrl(
+                        R.color.books,
+                        "https://fs01.androidpit.info/a/63/0e/android-l-wallpapers-630ea6-h900.jpg");
+            case 1:
+                headerLogo.setImageResource(R.drawable.thesis);
+                return HeaderDesign.fromColorResAndUrl(
+                        R.color.thesis,
+                        "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/06/wallpaper_51.jpg");
+            case 2:
+                headerLogo.setImageResource(R.drawable.musem);
+                return HeaderDesign.fromColorResAndUrl(
+                        R.color.musem,
+                        "http://www.droid-life.com/wp-content/uploads/2014/10/lollipop-wallpapers10.jpg");
+            case 3:
+                headerLogo.setImageResource(R.drawable.archieve);
+                return HeaderDesign.fromColorResAndUrl(
+                        R.color.archieve,
+                        "http://www.tothemobile.com/wp-content/uploads/2014/07/original.jpg");
+            case 4:
+                headerLogo.setImageResource(R.drawable.audiovideo);
+                return HeaderDesign.fromColorResAndUrl(
+                        R.color.audioVideo,
+                        "https://fs01.androidpit.info/a/63/0e/android-l-wallpapers-630ea6-h900.jpg");
+            case 5:
+                headerLogo.setImageResource(R.drawable.manuscript);
+                return HeaderDesign.fromColorResAndUrl(
+                        R.color.manuscript,
+                        "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/06/wallpaper_51.jpg");
+            case 6:
+                headerLogo.setImageResource(R.drawable.newspaper);
+                return HeaderDesign.fromColorResAndUrl(
+                        R.color.newspaper,
+                        "http://www.droid-life.com/wp-content/uploads/2014/10/lollipop-wallpapers10.jpg");
+            case 7:
+                headerLogo.setImageResource(R.drawable.map);
+                return HeaderDesign.fromColorResAndUrl(
+                        R.color.map,
+                        "http://www.tothemobile.com/wp-content/uploads/2014/07/original.jpg");
+        }
+        //execute others actions if needed //
+        return null;
+    }
+
+    private class CustomViewPagerAdapter extends FragmentStatePagerAdapter{
+
+        public CustomViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position % pageCount) {
+                //case 0:
+                //    return RecyclerViewFragment.newInstance();
+                //case 1:
+                //    return RecyclerViewFragment.newInstance();
+                //case 2:
+                //    return WebViewFragment.newInstance();
+                default:
+                    return RecyclerViewFragment.newInstance();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return pageCount;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position % pageCount) {
+                case 0:
+                    return getString(R.string.books);
+                case 1:
+                    return getString(R.string.thesis);
+                case 2:
+                    return getString(R.string.museum);
+                case 3:
+                    return getString(R.string.archieves);
+                case 4:
+                    return getString(R.string.audiovideo);
+                case 5:
+                    return getString(R.string.manuscripts);
+                case 6:
+                    return getString(R.string.newspaper);
+                case 7:
+                    return getString(R.string.maps);
+            }
+            return "";
+        }
     }
 
 }
