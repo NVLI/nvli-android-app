42ed5f687c776a20482524a381a22215cb570681
murtuza saifee <ermurtuzasaifee@gmail.com>
2016-07-16 21:19:27 +0400

    Book List handling

--- app/src/main/java/com/gov/iitnvli/home/FragmentBookDetail.java
+++ app/src/main/java/com/gov/iitnvli/home/FragmentBookDetail.java
@@ -1,19 +1,27 @@
 package com.gov.iitnvli.home;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.gov.iitnvli.R;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentBookDetail extends Fragment {
 
+    private View parentView;
+    private LandingActivity activity;
+    private ImageView bookImage;
+    private String imgUrl = "http://media.wiley.com/product_data/coverImage300/47/04704754/0470475447.jpg";
 
     public FragmentBookDetail() {
         // Required empty public constructor
@@ -23,8 +31,17 @@ public class FragmentBookDetail extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_detail, container, false);
+        activity = (LandingActivity) getActivity();
+
+        if (parentView != null){
+            return parentView;
+        }
+
+        parentView = inflater.inflate(R.layout.fragment_book_detail, container, false);
+
+        bookImage = (ImageView) parentView.findViewById(R.id.bookImage);
+        Picasso.with(activity).load(imgUrl).into(bookImage);
+        return parentView;
     }
 
 }
